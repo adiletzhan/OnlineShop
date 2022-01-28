5a75649eff30595d1bfef7ed1697dbfcfc0af6b0
adiletzhan <adiletzhan.kambetov@gmail.com>
2022-01-28 23:57:38 +0600

    recycler view

--- app/src/main/java/com/example/onlineshop/MainActivity.kt
+++ app/src/main/java/com/example/onlineshop/MainActivity.kt
@@ -2,16 +2,23 @@ package com.example.onlineshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.onlineshop.databinding.ActivityMainBinding
 import retrofit2.Response
+
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var retService: StoreService
+
+ //   private lateinit var products: StoreProducts
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,35 +29,35 @@ class MainActivity : AppCompatActivity() {
             .create(StoreService::class.java)
 
         getRequest()
+
     }
 
 
     private fun viewBinding(){
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
     }
 
     private fun getRequest(){
         val responseLiveData: LiveData<Response<StoreProducts>> = liveData {
-            //val response = retService.getAlbums()
             val response = retService.getProducts()
             emit(response)
-
         }
 
-        responseLiveData.observe(this, Observer {
-            val productsList: MutableListIterator<ProductsItem>? = it.body()?.listIterator()
-            if(productsList != null){
-                while(productsList.hasNext()){
-                    val productsItem: ProductsItem = productsList.next()
-                    val result = " " + "Product name: ${productsItem.title}" + "\n" +
-                            " " + "Product price: ${productsItem.price}" + "\n\n\n"
-                    binding.textView.append(result)
-                }
+        responseLiveData.observe(this, {
+
+            if(it.isSuccessful && it.body() != null) {
+                setupRecyclerView(it.body()!!)
+            //products = it.body()!!
+            } else {
+                Log.e(TAG, "Response not successful")
             }
-        })
+        }
+        )
+    }
+
+    private fun setupRecyclerView(products: StoreProducts) = binding.rvProducts.apply {
+        adapter = ProductAdapter(products)
+        layoutManager = LinearLayoutManager(this@MainActivity)
     }
 }
-
-//test
---
+++ app/src/main/java/com/example/onlineshop/ProductsAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.onlineshop
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.onlineshop.databinding.ItemProductBinding
+
+class ProductAdapter(private val products: StoreProducts): RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
+
+    inner class ProductViewHolder(val binding: ItemProductBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        return ProductViewHolder(ItemProductBinding.inflate(
+
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ))
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        holder.binding.apply {
+            val product = products[position]
+            tvTitle.text = product.title
+            tvPrice.text = product.price
+        }
+    }
+
+    override fun getItemCount(): Int = products.size
+
+}
